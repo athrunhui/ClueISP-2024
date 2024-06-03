c7b8b04d2bad76688d3e7fae0f381f482d99a032
Athrun Hui <ahui@bayviewglen.ca>
2024-06-03 10:26:09 -0400

    nothing

--- src/zork/guess.java
+++ src/zork/guess.java
@@ -1,7 +1,34 @@
 package zork;
 
 public class guess {
-    private Room murRoom;
-    private Item murWep;
-    private String murPers;
+    private static Room murRoom;
+    
+    private static Item murWep;
+    
+    private static String murPers;
+
+    public static Room getMurRoom() {
+        return murRoom;
+    }
+
+    public static void setMurRoom(Room murRoom) {
+        guess.murRoom = murRoom;
+    }
+
+    public static Item getMurWep() {
+        return murWep;
+    }
+    
+    public static void setMurWep(Item murWep) {
+        guess.murWep = murWep;
+    }
+
+    public static String getMurPers() {
+        return murPers;
+    }
+
+    public static void setMurPers(String murPers) {
+        guess.murPers = murPers;
+    }
+    
 }
